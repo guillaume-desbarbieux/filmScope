e354b272c124d8c7a450a9af8b396ec5165ebf01
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2026-03-24 15:30:32 +0100

    Move film data to Pinia store and update HomeView and FilmDetailView to use the store

--- src/views/FilmDetailView.vue
+++ src/views/FilmDetailView.vue
@@ -11,36 +11,12 @@
 <script setup>
 import { useRoute, useRouter } from 'vue-router'
 import { onMounted, ref } from 'vue'
+import { useFilmStore } from '@/stores/filmStore'
 
 const route = useRoute()
 const router = useRouter()
 
-const films = [
-  {
-    id: 1,
-    title: 'A New Hope',
-    year: 2000,
-    poster_url: 'https://placehold.co/300x100/orange/white?text=A+new+hope',
-    rating: 5.3,
-    isFavorite: false,
-  },
-  {
-    id: 2,
-    title: 'The Empire Strikes Back',
-    year: 2001,
-    poster_url: 'https://placehold.co/300x100/orange/white?text=The+empire+strikes+back',
-    rating: 8.2,
-    isFavorite: false,
-  },
-  {
-    id: 3,
-    title: 'Return of the Jedi',
-    year: 2003,
-    poster_url: 'https://placehold.co/300x100/orange/white?text=Return+of+the+Jedi',
-    rating: 7.8,
-    isFavorite: false,
-  },
-]
+const films = useFilmStore().films
 
 const film = ref(null)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,46 +1,22 @@
 <script setup>
 import { computed, ref } from 'vue'
+import {useFilmStore} from '@/stores/filmStore'
 import FilmCard from '@/components/FilmCard.vue'
 import SearchBar from '@/components/SearchBar.vue'
 
+const films = useFilmStore().films
 const appTitle = ref('FilmScope')
 const searchQuery = ref('')
 
-const films = ref([
-  {
-    id: 1,
-    title: 'A New Hope',
-    year: 2000,
-    poster_url: 'https://placehold.co/300x100/orange/white?text=A+new+hope',
-    rating: 5.3,
-    isFavorite: false,
-  },
-  {
-    id: 2,
-    title: 'The Empire Strikes Back',
-    year: 2001,
-    poster_url: 'https://placehold.co/300x100/orange/white?text=The+empire+strikes+back',
-    rating: 8.2,
-    isFavorite: false,
-  },
-  {
-    id: 3,
-    title: 'Return of the Jedi',
-    year: 2003,
-    poster_url: 'https://placehold.co/300x100/orange/white?text=Return+of+the+Jedi',
-    rating: 7.8,
-    isFavorite: false,
-  },
-])
 
 const filteredFilms = computed(() => {
-  return films.value.filter((film) =>
+  return films.filter((film) =>
     film.title.toLowerCase().includes(searchQuery.value.toLowerCase()),
   )
 })
 
 function toggleFavorite(film) {
-  const target = films.value.find((f) => f.id === film.id)
+  const target = films.find((f) => f.id === film.id)
   if (target) {
     target.isFavorite = !target.isFavorite
   }
