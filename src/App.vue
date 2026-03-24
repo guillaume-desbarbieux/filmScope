7a6ddec0c4ffe9b7e991998500c1ef66e713dc9d
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2026-03-24 15:13:34 +0100

    Add navbar with Home and Favorites links in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
 <template>
+  <div class="navbar">
+    <RouterLink to="/">Home</RouterLink>
+    <span> | </span>
+    <RouterLink to="/favorites">Favorites</RouterLink>
+  </div>
   <RouterView />
 </template>
