ed8bdab94aab70fa319f9d21fbbaeb5f1d47180b
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2026-03-24 15:22:07 +0100

    Extract Navbar into a separate AppNavbar component and integrate into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
+<script setup>
+import NavBar from './components/AppNavbar.vue'
+</script>
+
 <template>
-  <div class="navbar">
-    <RouterLink to="/">Home</RouterLink>
-    <span> | </span>
-    <RouterLink to="/favorites">Favorites</RouterLink>
-  </div>
+  <NavBar />
   <RouterView />
 </template>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav class="navbar">
+    <RouterLink to="/">Home</RouterLink>
+    <span> | </span>
+    <RouterLink to="/favorites">Favorites</RouterLink>
+  </nav>
+</template>
+
+<style scoped>
+.router-link-active {
+  font-weight: bold;
+  border-bottom: 2px solid currentColor;
+}
+</style>
